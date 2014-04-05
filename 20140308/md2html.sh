6e622f5f5afeb31259fba4faf896cb9d37f5c7d9
chuqq <chuqingq@qq.com>
2014-04-05 21:36:10 +0800

    tengine_cn_docs and test_timewait

--- 20140308/md2html.sh
+++
@@ -1,8 +0,0 @@
-docs="ngx_dso_module_cn ngx_http_concat_cn ngx_http_core_module_cn ngx_http_tfs_module_cn ngx_http_trim_filter_module_cn ngx_http_upstream_check_module_cn ngx_http_upstream_consistent_hash_module_cn ngx_http_upstream_session_sticky_module_cn ngx_procs_module_cn TFS_RESTful_API_cn"
-
-for f in $docs; do
-    echo "<html><head><meta charset=\"UTF-8\"></head>" > $f.html
-    markdown $f.md >> $f.html
-    echo "</html>" >> $f.html
-done
-
