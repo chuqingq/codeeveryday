f08e76421f0c4ded4e7d77593ff85e2d897466b8
unknown <c00334966@C00334966Z.china.huawei.com>
2016-04-20 15:14:43 +0800

    whole phantomjs multi page

--- 20160420_phantomjs_jquery_fs/test_phantomjs_jquery_fs.js
+++ 20160420_phantomjs_jquery_fs/test_phantomjs_jquery_fs.js
@@ -1,22 +1,48 @@
-var page = require('webpage').create();
+var webpage = require('webpage');
 var fs = require('fs');
 
-page.open('http://www.my285.com/wuxia/huangyi/xunqinji/423.htm', function() {
-	console.log('enter open');
+var start = 1;
+var stop = 730;
 
-    page.includeJs("http://lib.sinaapp.com/js/jquery/1.9.1/jquery-1.9.1.min.js", function() {
-        console.log('enter includeJs');
+function generateUrl(index) {
+    var indexStr = '' + index;
+    while (indexStr.length < 3) {
+        indexStr = '0' + indexStr;
+    }
+    return indexStr;
+}
 
-        var content = page.evaluate(function() {
-            console.log('enter evaluate');
-
-            return $('td[colspan="2"]').text();
-        });
-
-        fs.write('xqj1.txt', content, 'w');
-
+function capture(index) {
+    if (index > stop) {
+        console.log("exit");
         phantom.exit();
-    });
-	
-    console.log('after includeJs');
-});
+        return;
+    }
+
+    console.log('capture:', index);
+    var indexStr = generateUrl(index);
+    var page = webpage.create();
+    page.open('http://www.my285.com/wuxia/huangyi/xunqinji/'+indexStr+'.htm', function() {
+        console.log('enter open');
+
+        page.includeJs("http://lib.sinaapp.com/js/jquery/1.9.1/jquery-1.9.1.min.js", function() {
+            console.log('enter includeJs');
+
+            var content = page.evaluate(function() {
+                console.log('enter evaluate');
+
+                return $('td[colspan="2"]').text();
+            });
+            // console.log('after evaluate', content);
+
+            fs.write('xqj.txt', content+'\n\n', 'a');
+            page.close();
+            capture(index+1);
+            return;
+        });
+        
+        console.log('after includeJs');
+    });  
+}
+
+capture(start);
