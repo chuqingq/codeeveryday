d28b35b7eb0769d83d9be6bb441c9fadca9cbf39
chuqingq <chuqq@promote.cache-dns.local>
2016-07-17 21:35:04 +0800

    java maven

---
+++ 20160710_java_maven/myartifact/src/main/java/com/chuqq/mygroup/myartifact/App.java
@@ -0,0 +1,12 @@
+package com.chuqq.mygroup.myartifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("abc: " + System.getProperty("abc", "default"));
+	}
+}
---
+++ 20160710_java_maven/test.sh
@@ -0,0 +1,1 @@
+java -Dabc=1 -jar myartifact.jar
