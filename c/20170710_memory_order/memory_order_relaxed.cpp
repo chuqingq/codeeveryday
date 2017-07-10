00cad2bd2411ca3d81b32c85925bb854a85adacb
chuqingq <chuqingq@qq.com>
2017-07-10 22:29:45 +0800

    cpp memory order

---
+++ c/20170710_memory_order/memory_order_relaxed.cpp
@@ -0,0 +1,27 @@
+// g++ -std=c++11 -o memory_order_relaxed memory_order_relaxed.cpp -pthread
+#include <atomic>
+ #include <thread>
+ #include <assert.h>
+std::atomic<bool> x,y;
+std::atomic<int> z;
+void write_x_then_y()
+{
+  x.store(true,std::memory_order_relaxed);  // 1
+  y.store(true,std::memory_order_relaxed);  // 2
+}
+void read_y_then_x()
+{
+  while(!y.load(std::memory_order_relaxed));  // 3
+  if(x.load(std::memory_order_relaxed))  // 4
+++z;
+}
+int main() {
+  x=false;
+  y=false;
+  z=0;
+  std::thread a(write_x_then_y);
+  std::thread b(read_y_then_x);
+  a.join();
+b.join();
+  assert(z.load()==1);  // 5
+}
---
+++ c/20170710_memory_order/test.cpp
@@ -0,0 +1,28 @@
+// g++ -std=c++11 -o memory_order_release_acquire memory_order_release_acquire.cpp
+#include <atomic>
+#include <thread>
+#include <assert.h>
+std::atomic<bool> x,y;
+std::atomic<int> z;
+void write_x_then_y()
+{
+  x.store(true,std::memory_order_relaxed); // 1 自旋，等待y被设置为true
+  y.store(true,std::memory_order_relaxed);  // 2
+}
+void read_y_then_x()
+{
+  while(!y.load(std::memory_order_relaxed));  // 3
+  if(x.load(std::memory_order_relaxed))  // 4
+  ++z;
+}
+int main() {
+  x=false;
+  y=false;
+  z=0;
+  std::atomic_thread_fence(std::memory_order_acquire);
+  std::thread a(write_x_then_y);
+  std::thread b(read_y_then_x);
+  a.join();
+  b.join();
+  assert(z.load()!=0);  // 5
+}
