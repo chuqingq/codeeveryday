f4e50a50f81be42c416ee0e89b11f160969d4978
chuqq <chuqingq@qq.com>
2017-08-11 16:10:43 +0800

    go rawsocket

--- golang/20151207_go_http/rawsock_udp.go
+++ golang/20151207_go_http/rawsock_udp.go
@@ -3,7 +3,7 @@ package main
 import (
 	"log"
 	"net"
-	"time"
+	//"time"
 )
 
 func main() {
@@ -14,11 +14,8 @@ func main() {
 	defer conn.Close()
 
 	buf := make([]byte, 1024)
-	count := 0
-	const MAXCOUNT = 200000
-	before := time.Now()
 	for {
-		_, addr, err := conn.ReadFrom(buf)
+		n, addr, err := conn.ReadFrom(buf)
 		if err != nil {
 			log.Printf("ReadFrom error: %v\n", err)
 		}
@@ -27,21 +24,21 @@ func main() {
 		// 	continue
 		// }
 
-		if addr.String() == "127.0.0.1" {
+		if addr.String() != "127.0.0.1" {
+			continue
 			log.Printf("ReadFrom %v\n", addr)
 		}
-		count += 1
-		if count == MAXCOUNT {
-			log.Printf("recv: %f\n", float64(count)/time.Since(before).Seconds())
-			count = 0
-			before = time.Now()
-		}
+		log.Printf("ReadFrom %v, %v, %v\n", addr, n, string(buf[8:n]))
+
+		// conn.WriteTo(buf[:n], addr)
 	}
 }
 
 /*
-nc -u 127.0.0.1 12345
-123
+$ sudo ./rawsock_udp 
+2017/08/11 16:09:24 ReadFrom 127.0.0.1, 24, 123123123123123
 
-此时能看到有消息过来，日志打印ReadFrom: 127.0.0.1
+$ nc -u 127.0.0.1 12345
+123123123123123
+
 */
