388804eb4f904833627064e53411759182b8fe55
chuqingq <chuqingq@qq.com>
2017-09-01 20:02:28 +0800

    use fasthttp, 10 seconds for 100000 request 1 goroutine. result for ab is 9 seconds

---
+++ golang/20131104_go_helloworld_httpserver/go_fasthttp_client_bench.go
@@ -0,0 +1,61 @@
+
+package main
+
+import (
+	"log"
+	// "net"
+	// "io"
+	// "io/ioutil"
+	// "net/http"
+	"sync"
+	// "time"
+
+	"github.com/valyala/fasthttp"
+)
+
+var (
+	n   = 100000
+	c   = 1
+	url = "http://127.0.0.1:8081"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	for i := 0; i < c; i++ {
+		wg.Add(1)
+		go func() {
+			worker()
+			wg.Done()
+		}()
+	}
+	log.Printf("started...")
+
+	wg.Wait()
+}
+
+func worker() {
+	// client := &http.Client{}
+	client := &fasthttp.Client{}
+	buf := make([]byte, 1024)
+	for i := 0; i < n/c; i++ {
+		// res, err := client.Get(url)
+		_, _, err := client.Get(buf, url)
+		/*
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			log.Printf("newrequest error: %v", err)
+			continue
+		}
+		res, err := client.Do(req)
+		*/
+		if err != nil {
+			log.Printf("err: %v", err)
+			continue
+		}
+		// io.Copy(ioutil.Discard, res.Body)
+		// res.Body.Close()
+	}
+	log.Printf("concurrent finish")
+}
+
