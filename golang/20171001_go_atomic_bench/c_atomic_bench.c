e937dc0769f5f79def1f39d17d10eb154b98b8a2
chuqingq <chuqingq@qq.com>
2017-10-01 19:02:28 +0800

    format

--- golang/20171001_go_atomic_bench/c_atomic_bench.c
+++ golang/20171001_go_atomic_bench/c_atomic_bench.c
@@ -12,28 +12,28 @@ static long long ustime(void) {
 }
 
 int main() {
-	long long start, stop;
-	int value;
+    long long start, stop;
+    int value;
 
-	start = ustime();
-	for (int i = 0; i < 1000000000; i++) {
-		value++;
-	}
-	stop = ustime();
+    start = ustime();
+    for (int i = 0; i < 1000000000; i++) {
+        value++;
+    }
+    stop = ustime();
 
-	printf("%d\n", value);
-	printf("%lld\n", stop-start);
-	printf("use value++: %.6f\n", (stop-start)/1000000000.0);
+    printf("%d\n", value);
+    printf("%lld\n", stop-start);
+    printf("use value++: %.6f us/op\n", (stop-start)/1000000000.0);
 
 
-	value = 0;
-	start = ustime();
+    value = 0;
+    start = ustime();
         for (int i = 0; i < 1000000000; i++) {
-		__sync_fetch_and_add(&value, 1);;
+        __sync_fetch_and_add(&value, 1);;
         }
         stop = ustime();
 
         printf("%d\n", value);
         printf("%lld\n", stop-start);
-        printf("use atomicadd: %.6f\n", (stop-start)/1000000000.0);
+        printf("use atomicadd: %.6f us/op\n", (stop-start)/1000000000.0);
 }
