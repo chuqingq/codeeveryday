34981a3983f12c4b95310b5ef1d7142369bcb50f
chuqingq <chuqingq@qq.com>
2017-10-08 10:04:39 +0800

    add tests on ubuntu: clock_gettime can get nano seconds

--- c/20171008_ustime/nstime_sample.c
+++ c/20171008_ustime/nstime_sample.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-// #include <sys/time.h>
 #include <time.h>
 #include <unistd.h>
 
 static unsigned long long nstime(void) {
     struct timespec ts;
-    // long long ust;
-
     clock_gettime(CLOCK_REALTIME, &ts);
-    // ust = ((long)tv.tv_sec)*1000000;
-    // ust += tv.tv_usec;
-    // return ust;
-    return ((unsigned long long)ts.tv_sec)*1000000000 + ts.tv_nsec;
+    return ((unsigned long long)ts.tv_sec)*1e9 + ts.tv_nsec;
 }
 
 int main() {
@@ -22,14 +16,25 @@ int main() {
     t2 = nstime();
 
     printf("time diff: %llu\n", t2-t1);
-    printf("t1: %llu, t2: %llu, t3: %llu\n", t1, t2, t3);
+    printf("t1: %llu\nt3: %llu\nt2: %llu\n", t1, t3, t2);
     return 0;
 }
 
 /*
+# test on MPBr:
+
 ~/work/codeeveryday/c/20171008_ustime $ gcc nstime_sample.c
 ~/work/codeeveryday/c/20171008_ustime $ ./a.out
 time diff: 1005163000
 t1: 1507427455285188000, t2: 1507427456290351000, t3: 1507427455285188000
+
+# test on ubuntu:
+
+chuqq@chuqq-VPCS:~/work/codeeveryday/c/20171008_ustime$ gcc -O3 nstime_sample.c 
+chuqq@chuqq-VPCS:~/work/codeeveryday/c/20171008_ustime$ ./a.out 
+time diff: 1000167424
+t1: 1507428223080044288
+t3: 1507428223080044544
+t2: 1507428224080211712
 */
 
