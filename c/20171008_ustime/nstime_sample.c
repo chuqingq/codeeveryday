09871bc087d799ff971d75a1a12f309982abef60
chuqq <chuqingq@qq.com>
2017-10-20 08:55:16 +0800

    获取ns时间：即使开启-O3，也需要约256ns

--- c/20171008_ustime/nstime_sample.c
+++ c/20171008_ustime/nstime_sample.c
@@ -15,12 +15,29 @@ int main() {
     sleep(1);
     t2 = nstime();
 
+    printf("time diff1: %llu\n", t3-t1);
     printf("time diff: %llu\n", t2-t1);
     printf("t1: %llu\nt3: %llu\nt2: %llu\n", t1, t3, t2);
     return 0;
 }
 
 /*
+连续两次获取nanoseconds，大约花费250ns左右，无论是否开启-O3。
+
+chuqq@chuqq-hp:~/temp/codeeveryday/c/20171008_ustime$ ./a.out 
+time diff1: 256
+time diff: 1000087552
+t1: 1508460772922802688
+t3: 1508460772922802944
+t2: 1508460773922890240
+chuqq@chuqq-hp:~/temp/codeeveryday/c/20171008_ustime$ ./a.out 
+time diff1: 256
+time diff: 1000046848
+t1: 1508460779526198272
+t3: 1508460779526198528
+t2: 1508460780526245120
+
+
 # test on MPBr:
 
 ~/work/codeeveryday/c/20171008_ustime $ gcc nstime_sample.c
