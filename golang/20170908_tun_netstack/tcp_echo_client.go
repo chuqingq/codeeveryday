8e0ceb24193c398b12e29ff4932867b7ad048ca1
chuqq <chuqingq@qq.com>
2018-01-23 17:27:10 +0800

    更新测试数据，tcp的demo大约250us，ping大约25us

--- golang/20170908_tun_netstack/tcp_echo_client.go
+++ golang/20170908_tun_netstack/tcp_echo_client.go
@@ -7,18 +7,19 @@ import (
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "192.168.1.109:8888")
+	conn, err := net.Dial("tcp", "192.168.22.41:8888")
 	if err != nil {
 		panic(err.Error())
 	}
 	defer conn.Close()
 
 	buf := []byte("hello world")
-
 	count := 0
+	start := time.Now().UnixNano()
 	for {
 		//准备要发送的字符串
 		// msg := fmt.Sprintf("Hello World, %03d", i)
+		
 		n, err := conn.Write(buf)
 		if err != nil {
 			println("Write Buffer Error:", err.Error())
@@ -26,28 +27,55 @@ func main() {
 		}
 		// fmt.Printf("write: %s\n", msg)
 		if n != len(buf) {
-			fmt.Printf("write error len: %v", n)
-			return
+			println("Write len error:", n)
 		}
 
 		//从服务器端收字符串
-		n, err = conn.Read(buf)
+		n, err = conn.Read(buf[:])
 		if err != nil {
 			println("Read Buffer Error:", err.Error())
 			break
 		}
-		// fmt.Printf("read: %s\n", string(buf[0:n]))
+
 		if n != len(buf) {
-			fmt.Printf("write error len: %v", n)
+			println("Read len error:", n)
 		}
 
-		//等一秒钟
-		// time.Sleep(time.Second)
-
-		if count%10000 == 0 {
-			fmt.Printf("time: %v, count: %v\n", time.Now(), count)
+		// fmt.Printf("read: %s\n", string(buf[0:n]))
+		// fmt.Printf("%v", time.Now().UnixNano()-start)
+		count++
+		if count % 10000 == 0 {
+			now := time.Now().UnixNano()
+			fmt.Printf("elapsed: %v, avg: %v\n", now-start, (now-start)/10000)
+			start = now
 			count = 0
 		}
-		count += 1
 	}
 }
+
+// 单次
+// linux-qiwr:~ # ./tcp_echo_client 
+// read: hello world
+// 505230
+
+// 循环
+// linux-qiwr:~ # ./tcp_echo_client 
+// elapsed: 2452126940, avg: 245212
+// elapsed: 2452734333, avg: 245273
+// elapsed: 2658226987, avg: 265822
+// elapsed: 2656539814, avg: 265653
+// elapsed: 2451967858, avg: 245196
+// elapsed: 2656135622, avg: 265613
+// elapsed: 2454530446, avg: 245453
+// elapsed: 2452213075, avg: 245221
+// elapsed: 2653260511, avg: 265326
+// elapsed: 2658530878, avg: 265853
+
+// linux-qiwr:~ # ping 192.168.22.40
+// PING 192.168.22.40 (192.168.22.40) 56(84) bytes of data.
+// 64 bytes from 192.168.22.40: icmp_seq=1 ttl=64 time=0.026 ms
+// 64 bytes from 192.168.22.40: icmp_seq=2 ttl=64 time=0.027 ms
+// 64 bytes from 192.168.22.40: icmp_seq=3 ttl=64 time=0.026 ms
+// 64 bytes from 192.168.22.40: icmp_seq=4 ttl=64 time=0.027 ms
+// 64 bytes from 192.168.22.40: icmp_seq=5 ttl=64 time=0.022 ms
+
--- golang/20170908_tun_netstack/tun_new.sh
+++ golang/20170908_tun_netstack/tun_new.sh
@@ -4,7 +4,7 @@ sudo ip addr add 10.0.0.1/24 dev tun0
 
 sudo sysctl net.ipv4.ip_forward=1
 sudo iptables -P INPUT ACCEPT
-sudo iptables -t nat -A PREROUTING -p tcp -i enp0s8 --dport 8888 -j DNAT --to 10.0.0.2:8888
+sudo iptables -t nat -A PREROUTING -p tcp -i eth0 --dport 8888 -j DNAT --to 10.0.0.2:8888
 sudo iptables -t nat -A POSTROUTING -j MASQUERADE
 
 sudo ./tun_tcp_echo tun0 10.0.0.2 8888
