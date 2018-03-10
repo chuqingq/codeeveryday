b64f64bab60fdd5156bd7090315c3c33d422e6a4
chuqq <chuqingq@qq.com>
2018-03-10 12:35:18 +0800

    20~100us

--- c/20171010_pthread_mutex_cond/pthread_mutex_cond_delay.c
+++ c/20171010_pthread_mutex_cond/pthread_mutex_cond_delay.c
@@ -1,11 +1,10 @@
-// gcc pthread_cond_delay.c -O0 -pthread
-// strace ./a.out
+// gcc pthread_mutex_cond_delay.c -O3 -pthread
+// ./a.out
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <sys/time.h>
 
 #include <time.h>
 #include <unistd.h>
@@ -16,13 +15,6 @@ static unsigned long long nstime(void) {
     return ((unsigned long long)ts.tv_sec)*1000000000 + ts.tv_nsec;
 }
 
-
-static long long ustime(void) {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return ((long)tv.tv_sec)*1000000 + tv.tv_usec;
-}
-
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
@@ -30,14 +22,15 @@ long long int starttime;
 
 void* testThreadPool(int *t) {
 	// printf("thread start: %d\n", *t);
-	for (;;) {
-		// pthread_mutex_lock(&mutex);
+	// for (;;) {
+		pthread_mutex_lock(&mutex);
+		// printf("worker mutex\n");
 		pthread_cond_wait(&cond, &mutex);
 		// printf("pthread_cond_wait time: %llu\n", nstime());
 		printf("diff ns: %lld\n", nstime()-starttime);
 		pthread_mutex_unlock(&mutex);
 		// sleep(3);
-	}
+	// }
 	return (void*) 0;
 }
 
@@ -55,19 +48,24 @@ int main() {
 			printf("pthread_create");
 		}
 	}
-
-	sleep(20);
+	// printf("create thread\n");
+	sleep(1);
 
 	for (t = 0; t < thread_num; t++) {
 		// printf("pthread_cond_signal time: %llu\n", nstime());
 		starttime = nstime();
+                pthread_mutex_lock(&mutex);
 		// a = t;
 		pthread_cond_signal(&cond);
-		// pthread_mutex_unlock(&mutex);
+		pthread_mutex_unlock(&mutex);
+		// printf("after unlock\n");
 		// sleep(2);
 	}
 
-	sleep(2);
+	// sleep(2);
+        for (t = 0; t < thread_num; t++) {
+            pthread_join(mythread[t], NULL);
+        }
 }
 // 验证pthread_cond_signal唤醒线程需要多久：大约40~50us
 // chuqq-mbpr:
@@ -76,3 +74,16 @@ int main() {
 // virtualbox:
 // > ./a.out
 // diff ns: 141776
+
+
+/*
+chuqq@chuqq:~/work/codeeveryday/c/20171010_pthread_mutex_cond$ gcc pthread_mutex_cond_delay.c -O3 -pthread
+chuqq@chuqq:~/work/codeeveryday/c/20171010_pthread_mutex_cond$ ./a.out
+diff ns: 105583
+chuqq@chuqq:~/work/codeeveryday/c/20171010_pthread_mutex_cond$ ./a.out
+diff ns: 21451
+chuqq@chuqq:~/work/codeeveryday/c/20171010_pthread_mutex_cond$ ./a.out
+diff ns: 116579
+chuqq@chuqq:~/work/codeeveryday/c/20171010_pthread_mutex_cond$ ./a.out
+diff ns: 23621
+*/
