f79faaab48a644c01c17681c93b9986e649aa178
chuqq <chuqingq@qq.com>
2018-03-20 10:33:35 +0800

    明确condition_variable使用的unique_lock(mutex)的作用：确保调用cv.wait()的原子化

---
+++ c++/20180310_condition_var/condition_var2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>                // std::cout
+#include <thread>                // std::thread
+#include <mutex>                // std::mutex, std::unique_lock
+#include <condition_variable>    // std::condition_variable
+#include <chrono>
+
+std::mutex mtx; // 全局互斥锁.
+std::condition_variable cv; // 全局条件变量.
+bool ready = false; // 全局标志位.
+
+void do_print_id(int id)
+{
+    std::unique_lock <std::mutex> lck(mtx);
+    std::cout << "id: " << id << ", " << lck.owns_lock() << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    while (!ready) // 如果标志位不为 true, 则等待...
+        cv.wait(lck); // 当前线程被阻塞, 当全局标志位变为 true 之后,
+    // 线程被唤醒, 继续往下执行打印线程编号id.
+    std::cout << "thread " << id << '\n';
+}
+
+void go()
+{
+    // std::unique_lock <std::mutex> lck(mtx);
+    ready = true; // 设置全局标志位为 true.
+    cv.notify_all(); // 唤醒所有线程.
+}
+
+int main()
+{
+    std::thread threads[10];
+    // spawn 10 threads:
+    for (int i = 0; i < 10; ++i)
+        threads[i] = std::thread(do_print_id, i);
+
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    std::cout << "10 threads ready to race...\n";
+    go(); // go!
+
+    for (auto & th:threads)
+        th.join();
+
+    return 0;
+}
