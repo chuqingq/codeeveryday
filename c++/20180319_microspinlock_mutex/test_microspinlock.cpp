78705fe0160f8c067c459cf7d634f843b01b28d3
chuqq <chuqingq@qq.com>
2018-03-20 20:25:09 +0800

    microspinlock在物理机上不会卡住，效果没有futex稳定，最低36us左右

--- c++/20180319_microspinlock_mutex/test_microspinlock.cpp
+++ c++/20180319_microspinlock_mutex/test_microspinlock.cpp
@@ -11,16 +11,19 @@ int main() {
     MicroSpinLock lock;
     lock.lock();
     auto start = std::chrono::steady_clock::now();
+    // auto start = std::chrono::high_resolution_clock::now();
 
     std::thread t([&] {
         lock.lock();
         auto stop = std::chrono::steady_clock::now();
+	// auto stop = std::chrono::high_resolution_clock::now();
         std::cout << "elapsed: " << std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count() << " ns" << std::endl;
     });
 
     std::this_thread::sleep_for(std::chrono::milliseconds(0));
 
     start = std::chrono::steady_clock::now();
+    // start = std::chrono::high_resolution_clock::now();
     lock.unlock();
 
     t.join();
@@ -29,19 +32,19 @@ int main() {
 
 /*
 chuqq@chuqq-hp ~/t/c/c/20180319_microspinlock_mutex> ./a.out 
-elapsed: 37907 ns
+elapsed: 38761 ns
 chuqq@chuqq-hp ~/t/c/c/20180319_microspinlock_mutex> ./a.out 
-elapsed: 41949 ns
+elapsed: 37325 ns
 chuqq@chuqq-hp ~/t/c/c/20180319_microspinlock_mutex> ./a.out 
-elapsed: 36762 ns
+elapsed: 36449 ns
 chuqq@chuqq-hp ~/t/c/c/20180319_microspinlock_mutex> ./a.out 
-elapsed: 38443 ns
+elapsed: 38162 ns
 chuqq@chuqq-hp ~/t/c/c/20180319_microspinlock_mutex> ./a.out 
-elapsed: 36762 ns
+elapsed: 35964 ns
 chuqq@chuqq-hp ~/t/c/c/20180319_microspinlock_mutex> ./a.out 
-elapsed: 37700 ns
+elapsed: 16696 ns
 chuqq@chuqq-hp ~/t/c/c/20180319_microspinlock_mutex> ./a.out 
-elapsed: 36431 ns
+elapsed: 37801 ns
 chuqq@chuqq-hp ~/t/c/c/20180319_microspinlock_mutex> ./a.out 
-elapsed: 36932 ns
+elapsed: 12028 ns
 */
