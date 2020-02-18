465eba479718c7a8c5f9270d6ed57b9922bb12df
chuqq <chuqingq@qq.com>
2020-02-18 21:30:26 +0800

    matplotlib静态

---
+++ python/20200218_matplotlib/matplotlib_sample_2.py
@@ -0,0 +1,11 @@
+import numpy as np 
+from matplotlib import pyplot as plt 
+ 
+x = np.linspace(0, 10) 
+y = np.sin(x)
+plt.title("Matplotlib demo") 
+plt.xlabel("x axis caption") 
+plt.ylabel("y axis caption") 
+plt.plot(x,y)
+plt.show()
+
