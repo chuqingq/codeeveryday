225b0e08f5dc1957f67601088312a94385e0db5f
chuqq <chuqingq@qq.com>
2020-07-11 20:33:51 +0800

    原实现有误，直接操作mutex即可

--- golang/20200704_go_mutex_trylock/mutex.go
+++ golang/20200704_go_mutex_trylock/mutex.go
@@ -6,43 +6,14 @@ import (
 	"unsafe"
 )
 
-const (
-	LockedFlag   int32 = 1
-	UnlockedFlag int32 = 0
-)
+// https://colobu.com/2017/03/09/implement-TryLock-in-Go/
+
+const mutexLocked = 1 << iota
 
 type Mutex struct {
-	in     sync.Mutex
-	status *int32
-}
-
-func NewMutex() *Mutex {
-	status := UnlockedFlag
-	return &Mutex{
-		status: &status,
-	}
-}
-
-func (m *Mutex) Lock() {
-	m.in.Lock()
-}
-
-func (m *Mutex) Unlock() {
-	m.in.Unlock()
-	atomic.AddInt32(m.status, UnlockedFlag)
+	sync.Mutex
 }
 
 func (m *Mutex) TryLock() bool {
-	if atomic.CompareAndSwapInt32((*int32)(unsafe.Pointer(&m.in)), UnlockedFlag, LockedFlag) {
-		atomic.AddInt32(m.status, LockedFlag)
-		return true
-	}
-	return false
-}
-
-func (m *Mutex) IsLocked() bool {
-	if atomic.LoadInt32(m.status) == LockedFlag {
-		return true
-	}
-	return false
+	return atomic.CompareAndSwapInt32((*int32)(unsafe.Pointer(&m.Mutex)), 0, mutexLocked)
 }
