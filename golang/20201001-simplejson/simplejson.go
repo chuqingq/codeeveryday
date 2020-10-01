31e5e538e6f73d377f696939d11d70f5a411aa28
chuqq <chuqingq@qq.com>
2020-10-02 01:07:38 +0800

    simplejson跨级设置需要用到Json.MustMap()

--- golang/20201001-simplejson/simplejson.go
+++ golang/20201001-simplejson/simplejson.go
@@ -6,7 +6,7 @@ import (
 	json "github.com/bitly/go-simplejson"
 )
 
-// 验证go-simplejson支持按级别设置
+// 验证go-simplejson支持：某个path设置为Json，按照更深层次GetPath
 
 func main() {
 	j := json.New()
@@ -14,11 +14,20 @@ func main() {
 	k := json.New()
 	k.SetPath([]string{"a", "b"}, "b-data")
 
-	j.SetPath([]string{"a", "b", "d"}, k)
-	s, _ := j.Encode()
+	j.SetPath([]string{"a", "b", "d"}, k.MustMap(nil) /*这里用k不行*/)
+	s, _ := j.EncodePretty()
 	log.Printf("j: %v", string(s))
 
-	b := j.GetPath("a", "b")
-	s, _ = b.Encode()
+	b := j.GetPath("a", "b", "d", "a")
+	s, _ = b.EncodePretty()
 	log.Printf("b: %v", string(s))
+
+	/*
+		b2 := j.GetPath("a", "b", "d")
+		s, _ = b2.EncodePretty()
+		log.Printf("b2: %v", string(s))
+		b3 := b2.Get("a")
+		s, _ = b3.EncodePretty()
+		log.Printf("b3: %v", string(s))
+	*/
 }
