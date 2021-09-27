84183c0d7eb9f9a958004a58ccae580a741ae904
chuqq <chuqingq@qq.com>
2021-09-27 22:57:48 +0800

    InterfaceToBinaryBytes

---
+++ golang/util/binary.go
@@ -0,0 +1,14 @@
+package util
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+func InterfaceToBinaryBytes(vs ...interface{}) []byte {
+	var b bytes.Buffer
+	for _, v := range vs {
+		binary.Write(&b, binary.LittleEndian, v)
+	}
+	return b.Bytes()
+}
