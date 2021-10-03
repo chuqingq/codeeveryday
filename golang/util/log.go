9ee484462f2f7f73f3f256ae6f5836a7987c2641
chuqq <chuqingq@qq.com>
2021-10-03 20:41:35 +0800

    log on demand via udp

--- golang/util/log.go
+++ golang/util/log.go
@@ -2,14 +2,11 @@ package util
 
 import (
 	"log"
+	"net"
 	"os"
 )
 
-func init() {
-	log.SetFlags(log.Flags() | log.Lshortfile | log.Lmicroseconds)
-	logger = log.New(os.Stderr, "", log.Flags())
-}
-
+// 日志级别
 const (
 	LogLevelDebug = 0 + iota
 	LogLevelInfo
@@ -31,37 +28,83 @@ func SetLogLevel(level int) {
 	}
 }
 
-var empty emptyLogger
-
-var logger *log.Logger
-
 type printfInterface interface {
 	Printf(fmt string, v ...interface{})
-}
-
-type emptyLogger struct {
-}
-
-func (e *emptyLogger) Printf(fmt string, v ...interface{}) {
 }
 
 func D() printfInterface {
 	if curLevel <= LogLevelDebug {
 		return logger
 	}
-	return &empty
+	return empty
 }
 
 func I() printfInterface {
 	if curLevel <= LogLevelInfo {
 		return logger
 	}
-	return &empty
+	return empty
 }
 
 func E() printfInterface {
 	if curLevel <= LogLevelError {
 		return logger
 	}
-	return &empty
+	return empty
+}
+
+// 多个输出串联
+
+var empty *myLogger
+var logger *myLogger
+
+type myLogger struct {
+	loggers []*log.Logger
+}
+
+func (m *myLogger) Printf(fmt string, v ...interface{}) {
+	for _, l := range m.loggers {
+		l.Printf(fmt, v...)
+	}
+}
+
+func (m *myLogger) AppendLogger(logger ...*log.Logger) {
+	if m.loggers == nil {
+		m.loggers = make([]*log.Logger, 0, 2)
+	}
+	m.loggers = append(m.loggers, logger...)
+}
+
+// 输出方式
+
+var stdLogger *log.Logger
+
+func newStdLogger() *log.Logger {
+	return log.New(os.Stdout, "", log.Flags()|logFlags)
+}
+
+var udpLogger *log.Logger
+
+func newUdpLogger() *log.Logger {
+	conn, err := net.Dial("udp", "127.0.0.1:61234")
+	if err != nil {
+		log.Printf("log udp dial error: %v", err)
+		return nil
+	}
+	return log.New(conn, "", log.Flags()|logFlags)
+}
+
+const logFlags = log.Lshortfile | log.Lmicroseconds
+
+// init
+
+func init() {
+	// 输出方式
+	stdLogger = newStdLogger()
+	udpLogger = newUdpLogger()
+	// 级别：输出还是不输出
+	empty = &myLogger{}
+	empty.AppendLogger(udpLogger)
+	logger = &myLogger{}
+	logger.AppendLogger(stdLogger, udpLogger)
 }
--- golang/util/log_test.go
+++ golang/util/log_test.go
@@ -2,13 +2,13 @@ package util
 
 import "testing"
 
-func TestDebug(t *testing.T) {
+func TestLogDebug(t *testing.T) {
 	// default debug
 	D().Printf("debug log should be displayed")
 	I().Printf("info log should be displayed")
 }
 
-func TestWarn(t *testing.T) {
+func TestLogWarn(t *testing.T) {
 	SetLogLevel(LogLevelInfo)
 	D().Printf("!!!! debug log should not be displayed")
 	I().Printf("info log should be displayed")
