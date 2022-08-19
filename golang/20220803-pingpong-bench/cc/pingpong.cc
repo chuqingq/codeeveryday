ab15cd7de09ce26932553c522da7f4ccf00866cd
chuqq <chuqingq@qq.com>
2022-08-19 11:12:39 +0800

    pingpong condition_variable

---
+++ golang/20220803-pingpong-bench/cc/pingpong.cc
@@ -0,0 +1,137 @@
+/*
+ * @Author: baitian35 daniel.bai@dilu.com
+ * @Date: 2022-08-10 17:20:14
+ * @LastEditors: baitian35 daniel.bai@dilu.com
+ * @LastEditTime: 2022-08-19 10:19:20
+ * @FilePath: /cppp/alg/src/pingpong.cc
+ * @Description:
+ *
+ * Copyright (c) 2022 by 的卢技术有限公司, All Rights Reserved.
+ */
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+
+#include <iostream>
+//#include "my_common.h"
+
+void PingPong(uint32_t attempts) {
+  std::cout << "pingpong" << std::endl;
+
+  auto startTime = std::chrono::steady_clock::now();
+
+  std::atomic<uint32_t> vPong{attempts};
+  std::atomic<uint32_t> vPing{attempts};
+
+  std::thread ping([&] {
+    for (auto i = 0; i < attempts; ++i) {
+      vPong.store(i);
+      while (vPing.load() != i) {
+      }
+    }
+  });
+
+  std::thread pong([&] {
+    for (auto i = 0; i < attempts; ++i) {
+      while (vPong.load() != i) {
+      }
+      vPing.store(i);
+    }
+  });
+
+  // std::cout << "pingpong is running!" << std::endl;
+
+  ping.join();
+  pong.join();
+
+  auto endTime = std::chrono::steady_clock::now();
+
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+      endTime - startTime);
+
+  auto perDuration =
+      std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime)
+          .count() /
+      attempts;
+
+  std::cout << "Using Atomic pingpong test times:" << attempts
+            << " duration: " << duration.count() << "ms " << perDuration
+            << "ns/op" << std::endl;
+}
+
+void PingPong2(uint32_t attempts) {
+  std::cout << "pingpong" << std::endl;
+
+  auto startTime = std::chrono::steady_clock::now();
+
+  uint32_t pingTimes = attempts;
+  uint32_t pongTimes = attempts;
+
+  std::mutex pingMutex;
+  std::condition_variable pingVariable;
+
+  std::mutex pongMutex;
+  std::condition_variable pongVariable;
+
+  std::thread ping([&] {
+    for (auto i = 0; i < attempts; ++i) {
+      {
+        std::lock_guard<std::mutex> lk(pongMutex);
+        pongTimes = i;
+      }
+      pongVariable.notify_one();
+
+      std::unique_lock<std::mutex> pingLock(pingMutex);
+      pingVariable.wait(pingLock, [&] { return (pingTimes == i); });
+      pingLock.unlock();
+    }
+  });
+
+  std::thread pong([&] {
+    for (auto i = 0; i < attempts; ++i) {
+      std::unique_lock<std::mutex> pongLock(pongMutex);
+      pongVariable.wait(pongLock, [&] { return (pongTimes == i); });
+      pongLock.unlock();
+
+      {
+        std::lock_guard<std::mutex> lk(pingMutex);
+        pingTimes = i;
+      }
+      pingVariable.notify_one();
+    }
+  });
+
+  // std::cout << "pingpong is running!" << std::endl;
+
+  ping.join();
+  pong.join();
+
+  auto endTime = std::chrono::steady_clock::now();
+
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+      endTime - startTime);
+
+  auto perDuration =
+      std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime)
+          .count() /
+      attempts;
+
+  std::cout << "Using condtion_variable pingpong test times:" << attempts
+            << " duration: " << duration.count() << "ms " << perDuration
+            << "ns/op" << std::endl;
+}
+
+int main() {
+    const uint32_t attempts = 1000000;
+    PingPong2(attempts);
+    return 0;
+}
+
+/*
+$ ./a.out
+pingpong
+Using condtion_variable pingpong test times:1000000 duration: 29770ms 29770ns/op
+*/
+
