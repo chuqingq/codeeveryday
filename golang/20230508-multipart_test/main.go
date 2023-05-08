3fd9f25fa1a23700adaa539c813b96729b921abc
chuqq <chuqingq@qq.com>
2023-05-08 20:41:42 +0800

    multipart

--- golang/20230508-multipart_test/main.go
+++ golang/20230508-multipart_test/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
+	"fmt"
 	"io"
 	"log"
 	"net/textproto"
-	"regexp"
 )
 
 type MultipartReader struct {
@@ -19,7 +20,7 @@ func NewMultipartReader(reader io.Reader, boundary []byte) *MultipartReader {
 	return &MultipartReader{
 		reader:   reader,
 		boundary: boundary,
-		buffer:   make([]byte, 0, 1024*1024*16),
+		buffer:   make([]byte, 0, 1024*1024*4),
 	}
 }
 
@@ -39,64 +40,56 @@ func (mr *MultipartReader) NextPart() (*Part, error) {
 	}
 }
 
-var reContentLength = regexp.MustCompile(`Content-Length:.*\r\n`)
-
 // 尝试获取一个part，如果失败，返回nil
 func (mr *MultipartReader) tryGetPart() *Part {
-	// log.Printf("tryGetPart: bufferlen: %v", mr.bufferlen)
 	for mr.bufferlen > 0 {
 		ind := bytes.Index(mr.buffer[:mr.bufferlen], mr.boundary)
-		// log.Printf("ind: %v", ind)
 		if ind < 0 {
 			return nil
 		}
-		body := make([]byte, ind)
-		copy(body[:], mr.buffer[:ind])
-		body = bytes.Trim(body, "\r\n-")
-		// Content-Length
-		contentlength := reContentLength.Find(body)
-		if len(contentlength) > 0 {
-			log.Printf("%s", contentlength)
-		}
-		// headers
-		headerbody := bytes.SplitN(body, []byte("\r\n\r\n"), 2)
-		if len(headerbody) == 2 {
-			log.Printf("headers: %v", string(headerbody[0]))
-			log.Printf("body: %v", string(headerbody[1]))
+		data := append([]byte{}, mr.buffer[:ind]...)
+		data = bytes.Trim(data, "\r\n-")
+		// header and body
+		headerAndBody := bytes.SplitN(data, []byte("\r\n\r\n"), 2)
+		var headers textproto.MIMEHeader
+		var body []byte
+		var err error
+		if len(headerAndBody) == 2 {
+			// headers
+			headers, err = textproto.NewReader(bufio.NewReader(bytes.NewReader(append(headerAndBody[0], []byte("\r\n\r\n")...)))).ReadMIMEHeader()
+			if err != nil {
+				log.Printf("headers error: %v", err)
+			}
+			// body
+			body = headerAndBody[1]
 		}
 
+		// TODO
 		mr.bufferlen = copy(mr.buffer[:cap(mr.buffer)], mr.buffer[ind+len(mr.boundary):mr.bufferlen])
-		// log.Printf("bufferlen: %v", mr.bufferlen)
-		// if true /*mr.bufferlen > 10*/ {
-		// 	log.Printf("buffer: %v...", string(mr.buffer[:10]))
-		// }
 
-		if len(body) == 0 {
+		if len(data) == 0 {
 			continue
 		}
-
 		return &Part{
-			// TODO Header
-			Body: body,
+			Header: headers,
+			Body:   body,
 		}
 	}
 	return nil
 }
 
 type Part struct {
-	// TODO
 	Header textproto.MIMEHeader
-	// textproto.NewReader().ReadMIMEHeader()
-	Body []byte
+	Body   []byte
 }
 
 func main() {
 	var boundary = []byte("-----------------------7e13971310878")
 	content := MockFromFile()
 	// ioutil.WriteFile("content.txt", content, 0666)
+
 	mr := NewMultipartReader(bytes.NewReader(content), boundary)
-	count := 0
-	for {
+	for i := 1; ; i++ {
 		p, err := mr.NextPart()
 		if err == io.EOF {
 			log.Printf("EOF")
@@ -105,8 +98,20 @@ func main() {
 		if err != nil {
 			log.Fatalf("err: %v", err)
 		}
-		slurp := p.Body
-		count += 1
-		log.Printf("Part[%v]: Foo:%q, bodysize: %v\n", count, p.Header.Get("Foo"), len(slurp))
+
+		body := p.Body
+		var bodystr = ""
+		if len(body) > 6 {
+			bodystr = fmt.Sprintf("%v...%v", string(body[:6]), string(body[len(body)-6:]))
+		} else {
+
+		}
+		log.Printf("Part[%v]: Content-Length:%q, bodysize: %v, body: %v\n", i, p.Header.Get("Content-Length"), len(body), bodystr)
 	}
 }
+
+// 2023/05/08 20:39:05 Part[36]: Content-Length:"506", bodysize: 504, body: <Event...Alert>
+// 2023/05/08 20:39:05 Part[37]: Content-Length:"4123", bodysize: 4121, body: <?xml ...Alert>
+// 2023/05/08 20:39:05 Part[38]: Content-Length:"24708", bodysize: 24708, body: �����...x/���
+// 2023/05/08 20:39:05 Part[39]: Content-Length:"417908", bodysize: 417908, body: ����j...�g��
+// 2023/05/08 20:39:05 EOF
