2f27d03ec2be4d4f41a55f432dd67e776d07acae
chuqq <chuqingq@qq.com>
2024-04-22 17:24:22 +0800

    transtream_wrapper

--- python/20240406_python_opencv/transtream_wrapper.py
+++ python/20240406_python_opencv/transtream_wrapper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import signal
 import sys
@@ -7,84 +7,74 @@ import logging
 
 
 logging.basicConfig(
-    filename='transtream_wrapper.log',
+    # filename="transtream_wrapper.log",
     level=logging.DEBUG,
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
 
 
-transtream_proc = None
-
-
-async def transtream_inout():
-    """处理输入输出"""
-    logging.debug("start transtream_inout...")
-    # asyncio.get_event_loop().add_reader(sys.stdin, input_handler)
-    # 等待transtream进程启动
-    asyncio.sleep(1)
-    while True:
-        input_text = await asyncio.get_event_loop().run_in_executor(None, input)
-        if transtream_proc:
-            transtream_proc.stdin.write(input_text.encode())
-            await transtream_proc.stdin.drain()
-            logging.debug(f"transtream_proc.stdin.write({input_text})")
-
-
-# 启动transtream
+# 执行transtream，直到进程退出
 async def transtream():
-    """启动transtream"""
     logging.debug("start transtream...")
     proc = await asyncio.create_subprocess_shell(
         "./do_transtream",
         stdin=asyncio.subprocess.PIPE,
-        # stdout=asyncio.subprocess.PIPE,
+        stdout=asyncio.subprocess.PIPE,
         # stderr=asyncio.subprocess.PIPE,
     )
-    global transtream_proc
-    transtream_proc = proc
-    # 处理标准输出
-    # task_stdout = asyncio.create_task(handle_transtream_stdout(proc.stdout))
-    # 处理标准错误（日志）
-    # task_stderr = asyncio.create_task(handle_transtream_stderr(proc.stderr))
-    # 传入标准输入
-    # proc.stdin.writelines([b'{"a":456,"b":7,"c":8}\n'])
-    # await proc.stdin.drain()
-    # 等待任务完成
-    await asyncio.gather(proc.wait()) # , task_stdout, task_stderr
-    logging.debug("transtream finished.")
-    asyncio.get_event_loop().stop()
+    await asyncio.gather(handle_stdout(proc), handle_stdin(proc), proc.wait())
 
 
-async def handle_transtream_stdout(stream):
-    """处理标准输出"""
-    # logging.debug("handle_transtream_stdout")
+# 处理标准输入
+async def handle_stdin(proc):
+    logging.debug("start handle_transtream_stdin..")
+    # 创建一个 asyncio.StreamReader 对象
+    stdin_reader = asyncio.StreamReader()
+    # 创建一个 asyncio.StreamReaderProtocol 对象，并将其绑定到标准输入流上
+    reader_protocol = asyncio.StreamReaderProtocol(stdin_reader)
+    await asyncio.get_event_loop().connect_read_pipe(lambda: reader_protocol, sys.stdin)
+    # 从标准输入流读取数据
     while True:
-        line = await stream.readline()
-        # logging.debug(f"transtream stdout: {line}")
+        try:
+            # 读取一行输入
+            line = await stdin_reader.read(1024)
+            if not line:
+                logging.info("input EOF")
+                break  # 如果没有更多输入，则退出循环
+            # 处理输入数据
+            proc.stdin.write(line)
+            await proc.stdin.drain()
+        except KeyboardInterrupt:
+            # 捕获键盘中断（Ctrl+C），退出循环
+            break
+    # 结束进程，即关闭进程的标准输入
+    proc.stdin.close()
+    await proc.stdin.wait_closed()
+    logging.info("end handle_transtream_stdin")
+
+
+# 处理标准输出
+async def handle_stdout(proc):
+    """处理标准输出"""
+    logging.debug("handle_transtream_stdout")
+    while True:
+        line = await proc.stdout.read(1024)
         if not line:
             break
-        # logging.debug("transtream stdout: " + line.decode().strip())
+        logging.debug("transtream stdout: " + line.decode().strip())
         # 打印出来给父进程使用
-        print(line.decode(), '\n')
+        print(line.decode().strip())
+    logging.info("end handle_transtream_stdout")
 
 
-async def handle_transtream_stderr(stream):
+# 处理标准错误
+async def handle_transtream_stderr(proc):
     """处理标准错误"""
     while True:
-        line = await stream.readline()
+        line = await proc.stderr.read(1024)
         if not line:
             break
-        logging.debug("transtream stderr: " + line.decode().strip())
-
-
-async def main():
-    """主函数"""
-    # 启动transtream
-    task_transtream = asyncio.create_task(transtream())
-    # 处理输入输出
-    task_inout = asyncio.create_task(transtream_inout())
-    # 等待任务完成
-    await asyncio.gather(task_inout, task_transtream)
+        logging.error("transtream stderr: " + line.decode().strip())
 
 
 def signal_handler(sig, frame):
@@ -96,4 +86,4 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(transtream())
